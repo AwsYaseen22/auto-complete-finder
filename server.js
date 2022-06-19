dccd1d4dcf94dc1cff26a5cb9a765cbfda8520a3
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-06-19 23:54:58 +0300

    add second route and create the index.html

--- server.js
+++ server.js
@@ -22,4 +22,39 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.get("/search", async (req, res) => {
+  try {
+    let result = await collection
+      .aggregate([
+        {
+          $Search: {
+            autocomplete: {
+              query: `${req.query.query}`,
+              path: "title",
+              fuzzy: {
+                maxEdits: 2,
+                prefixLength: 3,
+              },
+            },
+          },
+        },
+      ])
+      .toArray();
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/get/:id", async (req, res) => {
+  try {
+    let result = await collection.findOne({
+      _id: ObjectId(req.params.id),
+    });
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
