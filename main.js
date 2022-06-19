dc44b99051b5e429e0f0dae50c5696063f28fd15
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-06-20 02:01:15 +0300

    almost working not showing data yet

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 $(document).ready(function () {
-  $("#title").autocomplete({
-    source: async function (req, res) {
-      let data = await fetch(`http://localhost:8000/search?query=${req.term}`);
-      let result = await data.json();
-      result = result.map((e) => {
-        return {
-          label: e.title,
-          value: e.title,
-          id: e._id,
-        };
-      });
-      res(result);
-    },
-    minLength: 2,
-    select: function (event, ui) {
-      console.log(ui.item.id);
-      fetch(`http://localhost:8000/get/${ui.item.id}`)
-        .then((result) => result.json())
-        .then((result) => {
-          $("#cast").empty();
-          result.cast.forEach((cast) => {
-            $(cast).append(`<li>${cast}</li>`);
-            $("img").attr("src", result.poster);
-          });
-        });
-    },
-  });
-});
+    $('#title').autocomplete({
+        source: async function(request,response) {
+            let data= await fetch(`http://localhost:8000/search?query=${request.term}`)
+                    .then(results => results.json())
+                    .then(results => results.map(result => {
+                        return {
+                            label: result.title,
+                            value: result.title,
+                            id: result._id
+                        }
+                    }))
+                response(data)
+                console.log(data)
+        },
+        minLength: 2,
+        select: function(event, ui) {
+            console.log(ui.item.id)
+            fetch(`http://localhost:8000/get/${ui.item.id}`)
+                .then(result => result.json())
+                .then(result => {
+                    $('#cast').empty()
+                    result.cast.forEach(cast =>
+                        {
+                            $("#cast").append(`<li>${cast}</li>`)
+                        })
+                        $('img').attr('src',result.poster)
+                })
+        }
+    })
+})
