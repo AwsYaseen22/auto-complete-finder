d2902bf36f521c6df2fe36411758e782a5e78bf8
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-06-19 23:05:08 +0300

    run the server and connected to the DB

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+require("dotenv").config();
+
+let db,
+  dbConnectionStr = process.env.DB_URI,
+  dbName = "sample_mflix",
+  collection;
+const PORT = process.env.PORT || 8000;
+
+MongoClient.connect(dbConnectionStr).then((client) => {
+  console.log(`connected to database`);
+  db = client.db(dbName);
+  collection = db.collection("movies");
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
